55da88a59bb251c3adbab798b544d00794d3e43e
Anderson David <Anderson@Andersons-MacBook-Air.local>
2018-12-08 15:26:22 -0500

    got user logo

--- src/components/topPageStripe.js
+++ src/components/topPageStripe.js
@@ -3,6 +3,7 @@ import pageBackgroundStyles from "./pageBackground.module.css"
 import '../fonts/PenumbraFlareStd-Semibold.otf'
 import { Link } from "gatsby"
 import { backgroundColor, borderColor } from "./imageContainer"
+import UserLogo from "../images/GenericUser.png"
 
 
 var textColor = borderColor === "#ffffff" ? "#000000" : "#ffffff"
@@ -12,9 +13,17 @@ class TopPageStripe extends React.Component {
     render() {
         return (
         <div className={pageBackgroundStyles.topPageStripe} style={{ backgroundColor: stripeColor }}>
-            <Link to="/" style={{ textDecoration: 'none', color: textColor }}>
-                LOGIC CENTRAL
-            </Link>
+            <div style={{ color: textColor, flexGrow: "1" }}>
+                
+            </div>
+            <div style={{ flexGrow: "1", textAlign: "center" }}>
+                <Link to="/" style={{ textDecoration: 'none', color: textColor }}>
+                    LOGIC CENTRAL
+                </Link>
+            </div>
+            <div style={{ color: textColor, flexGrow: "1", textAlign: "right" }}>
+                <img style={{ height: "40px" }} alt="u" src={UserLogo}/>
+            </div>
         </div>
         )
     }
